efd3c7ddd4c7f43e7de31b2dea4bfe2d361e7604
Michael Nishizawa <michael.nishizawa@spectrumhealth.org>
2014-04-30 15:59:11 -0400

    spec passes

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,8 @@
-# This is a placeholder class.
-class template {
+# SSHConfig 
+# Reads a user's ssh config file to 
+#   - if specified, move the user's sshconfig file in place
+#   - create the ssh key
+#   - distribute it to all the servers in the user's sshconfig file
+class sshconfig {
   anchor { 'Hello_World': }
 }
